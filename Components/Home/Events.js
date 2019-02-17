0fe00584f6607fef2df746cc3ec8f8f391b4ccb8
Anil <anil@Anils-MacBook-Pro.local>
2019-02-18 00:10:17 +0530

    collectionview add

--- Components/Home/Events.js
+++ Components/Home/Events.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { StyleSheet, ImageBackground, View, Text, TextInput, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, ImageBackground, View, Text, TextInput, FlatList, Dimensions,TouchableOpacity, Image, SafeAreaView } from 'react-native';
+
+const data = [
+    { key: 'A' }, { key: 'B' }, { key: 'C' }, { key: 'D' }, { key: 'E' }, { key: 'F' }, { key: 'G' }, { key: 'H' }, { key: 'I' }, { key: 'J' },
+    // { key: 'K' },
+    // { key: 'L' },
+  ];
+  
+  const formatData = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
+  
+    let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+    while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+      data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+      numberOfElementsLastRow++;
+    }
+  
+    return data;
+  };
+  
+  const numColumns = 3;
 
 export default class Home extends React.Component {
 
     _onSIGNINPressed() {
         this.props.navigation.openDrawer();
     }
+
+    renderItem = ({ item, index }) => {
+        if (item.empty === true) {
+          return <View style={[styles.item, styles.itemInvisible]} />;
+        }
+        return (
+          <View
+            style={styles.item}
+          >
+            <Text style={styles.itemText}>{item.key}</Text>
+          </View>
+        );
+      };
 
     render() {
 
@@ -16,6 +49,12 @@ export default class Home extends React.Component {
                 <TouchableOpacity onPress={() => this._onSIGNINPressed()}>
                     <Image style={styles.backButton} source={require('../../resources/Tabbar/menu.png')} />
                 </TouchableOpacity>
+                <FlatList
+        data={formatData(data, numColumns)}
+        style={styles.container}
+        renderItem={this.renderItem}
+        numColumns={numColumns}
+      />
                 </View>
                 </ImageBackground>
                 </SafeAreaView>
@@ -32,5 +71,18 @@ var styles = StyleSheet.create({
         width: 35,
         textAlign: 'left',
         resizeMode: 'stretch',
-    },
+    }, item: {
+        backgroundColor: '#4D243D',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        margin: 1,
+        height: Dimensions.get('window').width / numColumns, // approximate a square
+      },
+      itemInvisible: {
+        backgroundColor: 'transparent',
+      },
+      itemText: {
+        color: '#fff',
+      },
 })
